23568c45bdbc7fd4816c8fe9f6ad26cb06b39e59
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2024-12-26 13:50:59 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -0,0 +1,41 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+
+        def binSearch(nums, target):
+            l = 0
+            r = len(nums) - 1
+            while l <= r:
+                m = (l + r) //2
+
+                if nums[m] == target:
+                    return m
+                elif nums[m] < target:
+                    l = m + 1
+                else:
+                    r = m - 1
+            return -1
+        
+
+        l = 0
+        r = len(nums) - 1
+        m = 0
+        while l < r:
+            leftMost = nums[l]
+            rightMost = nums[r]
+            m = (l + r)//2
+
+            if r - l == 1:
+                m = l
+                break            
+            if leftMost > nums[m]:
+                r = m 
+            if rightMost < nums[m]:
+                l = m 
+        print(nums[0:m + 1])
+        print(nums[m+1:])
+        left = binSearch(nums[0: m + 1], target)
+        right = binSearch(nums[m+1 :], target)
+        if right != -1:
+            right += m + 1
+        return max(left,right)
+            
