25e41d1cb087adcdd51bf42ce27a948008c31ba8
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2024-12-30 14:58:08 -0500

    Time: 141 ms (14.22%), Space: 20.7 MB (65.78%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -18,27 +18,29 @@ class Solution:
                         queue.append([row, col -1,  1])
                     if col + 1 < len(rooms[0]):
                         queue.append([row, col + 1, 1])
+
                     seen = set()
                     while queue:
                         curr = queue.popleft()
-                        row = curr[0]
-                        col = curr[1]
+                        row2 = curr[0]
+                        col2 = curr[1]
                         distance = curr[2]
-                        if rooms[row][col] == 0 or rooms[row][col] == -1 or (row,col) in seen:
+                        if rooms[row2][col2] == 0 or rooms[row2][col2] == -1 or (row2,col2) in seen:
                             continue
-                        if distance < rooms[row][col]:
-                            rooms[row][col] = distance
+                        if distance < rooms[row2][col2]:
+                            rooms[row2][col2] = distance
                         
-                        seen.add((row,col))
-                        if row -1 >= 0:
-                            queue.append([row - 1, col, distance + 1])
-                        if row + 1 < len(rooms):
-                            queue.append([row + 1, col, distance + 1])
-                        if col - 1 >= 0:
-                            queue.append([row, col -1, distance + 1])
-                        if col + 1 < len(rooms[0]):
-                            queue.append([row, col + 1, distance + 1])
+                        seen.add((row2,col2))
+                        if row2 -1 >= 0:
+                            queue.append([row2 - 1, col2, distance + 1])
+                        if row2 + 1 < len(rooms):
+                            queue.append([row2 + 1, col2, distance + 1])
+                        if col2 - 1 >= 0:
+                            queue.append([row2, col2 - 1, distance + 1])
+                        if col2 + 1 < len(rooms[0]):
+                            queue.append([row2, col2 + 1, distance + 1])
+                        
             
             
 
-            
+    
