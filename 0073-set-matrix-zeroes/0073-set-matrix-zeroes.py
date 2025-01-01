2a331c266cd11d17f66c758a0a63b4edca9f0f92
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2024-12-31 19:06:03 -0800

    Time: 9 ms (9.77%), Space: 18.4 MB (15.18%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,20 +3,30 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        zeroes = []
+        
+        def fix(row,col, matrix):
+            for i in range(len(matrix)):
+                if matrix[i][col] == 0:
+                    matrix[i][col] = "True"
+                    fix(i,col,matrix)
+                matrix[i][col] = "True"
+            for i in range(len(matrix[0])):
+                if matrix[row][i]== 0:
+                    matrix[row][i] = "True"
+                    fix(row,i,matrix)
+                matrix[row][i] = "True"
 
         for row in range(len(matrix)):
             for col in range(len(matrix[0])):
                 if matrix[row][col] == 0:
-                    zeroes.append((row,col))
+                    fix(row,col,matrix)
 
-        def fix(row,col, matrix):
-            for i in range(len(matrix)):
-                matrix[i][col] = 0
+
             
-            for i in range(len(matrix[0])):
-                matrix[row][i] = 0
+
         
-        for zero in zeroes:
-            fix(zero[0], zero[1], matrix)
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == "True":
+                    matrix[row][col] = 0
         
