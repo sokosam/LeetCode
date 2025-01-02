642c5104e93687856d08297799328fd651bf90e5
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-02 11:35:51 -0800

    Time: 0 ms (100%), Space: 18.9 MB (30.51%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.py
+++ 0207-course-schedule/0207-course-schedule.py
@@ -1,33 +1,26 @@
 class Solution:
-    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        courses = {}
+    def canFinish(self, numCourses, prerequisites):
+        indegree = [0]* numCourses
+        adj = [[] for _ in range(numCourses)]
 
-        for pre in prerequisites:
-            if pre[0] not in courses:
-                courses[pre[0]] = [pre[1]]
-            else:
-                courses[pre[0]].append(pre[1])
-        
-        works = set()
-        def check(seen, course, works, courses):
-            if course in works or course not in courses:
-                return True
-            if seen[course]:
-                return False
-            
-            seen[course]= True
-            ans = True
-            for i in courses[course]:
-                ans = ans and check(seen,i,works,courses)
-            seen[course] = False
-            return ans
-        seen = [False]* numCourses
+        queue = deque()
+        nodesVisited = 0
+
+        for req in prerequisites:
+            indegree[req[0]] += 1
+            adj[req[1]].append(req[0])
+
         for i in range(numCourses):
-            if i not in courses: continue
+            if indegree[i] == 0:
+                queue.append(i)
 
-            if check(seen,i,works,courses):
-                works.add(i)
-            else:
-                return False
+        while queue:
+            nodesVisited +=1
+            curr = queue.popleft()
 
-        return True
+            for i in adj[curr]:
+                indegree[i] -= 1
+                if indegree[i] == 0:
+                    queue.append(i)
+        
+        return nodesVisited == numCourses
