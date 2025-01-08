93e55eabb90170828330bcaf3aea159c419317bb
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-07 16:45:17 -0800

    Time: 0 ms (100%), Space: 19 MB (14.1%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.py
@@ -1,12 +1,5 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        """
-        7 1 5 3 6 4
-        7 1 1 1 1 1
-        7 6 6 6 6 4
-
-
-        """
 
         total = 0 
         curr = float('inf')
