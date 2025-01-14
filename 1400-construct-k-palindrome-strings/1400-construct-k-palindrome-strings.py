ef64c27dc525a3fddaa55bb4e99d5f495d9e8f30
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-13 22:04:16 -0500

    Time: 110 ms (7.2%), Space: 18 MB (94.87%) - LeetHub

--- 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
+++ 1400-construct-k-palindrome-strings/1400-construct-k-palindrome-strings.py
@@ -5,28 +5,21 @@ class Solution:
 
         chars = [0] * 26
 
-        for i in  s:
+        numOdd = 0
+
+        for i in s:
             chars[ord(i) - ord('a')] +=1
+            val = chars[ord(i)-  ord('a')]
+            if val% 2 == 1:
+                numOdd += 1
+            else:
+                numOdd -= 1
 
     
-        numOdd = 0
-        numEven = 0
-        single = 0
-
-        for i in range(26):
-            if chars[i] == 1:
-                single += 1
-            elif chars[i] % 2 == 0:
-                numEven += 1
-            else:
-                numOdd += 1
 
 
-
-
-        if single + numOdd> k:
+        if  numOdd> k:
             return False
-
         return True
 
 
