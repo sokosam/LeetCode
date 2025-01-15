323efa9fcc20fd79929dc272d13ec918ce2b42d9
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-14 21:42:22 -0500

    Time: 0 ms (100%), Space: 18 MB (5.82%) - LeetHub

---
+++ 1652-defuse-the-bomb/1652-defuse-the-bomb.py
@@ -0,0 +1,29 @@
+class Solution:
+    def decrypt(self, code: List[int], k: int) -> List[int]:
+        if k == 0: return [0] * len(code)
+
+        ans = []
+        if k > 0:
+            window = 0
+            for i in range(k):
+                window += code[i]
+            k-=1
+            for i in range(len(code)):
+                window -= code[i]
+                k += 1
+                if k >=len(code):
+                    k = 0
+                window += code[k]
+                ans.append(window)
+        else:
+            window = 0
+            for i in range(k - 1 , -1):
+                window += code[i]
+            print(window)
+            for i in range(len(code)):
+                window += code[i - 1]
+                window -= code[k -1]
+                k+= 1
+                ans.append(window)
+            
+        return ans
