c58b18f1e6c0dc17774666e5f15e7ad8a5246443
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-15 20:02:57 -0500

    Time: 0 ms (100%), Space: 36.4 MB (60.69%) - LeetHub

--- 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -3,25 +3,22 @@ class Solution:
         
         
         count = {}
-
-        for i in nums2:
-            if i not in count:
-                count[i] = len(nums1)
-            else:
-                count[i] += len(nums1)
-
-        for i in nums1:
-            if i not in count:
-                count[i] =  len(nums2)
-            else:
-                count[i] += len(nums2)
-        
         ans = 0
-        for num in count:
-            if count[num] % 2 == 1:
-                ans ^= num
-        print(count)
+        if len(nums2) % 2 == 1:
+            for i in nums1:
+                ans ^= i
+        if len(nums1) % 2 == 1:
+            for i in nums2:
+                ans^= i
         return ans
+
+
+        
+        # ans = 0
+        # for num in count:
+        #     if count[num] % 2 == 1:
+        #         ans ^= num
+        # return ans
         # [2,1,3] [10,2,5,0] 
         # 2^10 2^2 2^5 2^0 1^10 1^2 1^5 1^0 3^10 3^2 3^5 3^0 
         #  1   3   4   5       6                7
