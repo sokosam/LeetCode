35af496c10777eb8f150a5e5a9f506e48679bed1
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-15 19:43:31 -0500

    Time: 95 ms (5.2%), Space: 39 MB (7.51%) - LeetHub

---
+++ 2425-bitwise-xor-of-all-pairings/2425-bitwise-xor-of-all-pairings.py
@@ -0,0 +1,37 @@
+class Solution:
+    def xorAllNums(self, nums1: List[int], nums2: List[int]) -> int:
+        
+        
+        count = {}
+
+        for i in nums2:
+            if i not in count:
+                count[i] = len(nums1)
+            else:
+                count[i] += len(nums1)
+
+        for i in nums1:
+            if i not in count:
+                count[i] =  len(nums2)
+            else:
+                count[i] += len(nums2)
+        
+        ans = 0
+        for num in count:
+            if count[num] % 2 == 1:
+                ans ^= num
+        print(count)
+        return ans
+        # [2,1,3] [10,2,5,0] 
+        # 2^10 2^2 2^5 2^0 1^10 1^2 1^5 1^0 3^10 3^2 3^5 3^0 
+        #  1   3   4   5       6                7
+
+        # [8,0,7,..... ]
+        #   ^
+
+        # (2^10) ^ 2 ^ 2
+        # = 0
+
+        # parity of the counts of numbers
+        # if it is even it ends up adding a final sum zero
+        # if it is odd then it is just x
