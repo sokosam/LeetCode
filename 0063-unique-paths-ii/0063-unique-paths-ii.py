462968da75c86a2edffb7e2b738cab0414d18748
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-29 17:00:24 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0063-unique-paths-ii/0063-unique-paths-ii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        dp = [[0 for _ in range(len(obstacleGrid[0]) + 1)] for _ in range(len(obstacleGrid) + 1)]
+
+
+        dp[1][1] = 1
+        for row in range(1, len(dp)):
+            for col in range(1, len(dp[0])):
+                if row == 1 and col ==1: continue
+                left = dp[row ][col -1 ]
+                top =dp[row - 1][col]
+
+                if obstacleGrid[row-1][col-1] == 1:
+                    dp[row][col] = 0
+                else:
+                    dp[row][col] = left + top
+        
+        print(dp)
+        return dp[-1][-1]
