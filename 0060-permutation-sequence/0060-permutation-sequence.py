3e940a60d2a12605c23912137a27ca10b8f15a49
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-29 16:53:12 -0500

    Time: 3 ms (33%), Space: 18.1 MB (18.73%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,31 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        fact = [1,1,2,6,24,120,720,5040,40320,362880]
+        # curr = 1
+        # for i in range(1, 9 + 1):
+        #     curr*= i
+        #     print(curr)
+        
+
+        m = [x for x in range(10)]
+
+        def getDigit(k, n):
+            if (n ) == 0 or k == 0: return ""
+            val = (k - 1)//fact[n-1] + 1
+            print(val)
+            if val == 0: val = 1
+            
+            temp = val
+            val = m[val]
+            
+            m.pop(temp)
+        
+            return str(val) + getDigit(k%fact[n-1], n -1)
+        res = getDigit(k,n)
+        print(res)
+        for i in range(len(m) - 1, 0, -1):
+            if m[i] <= n and str(m[i]) not in res and len(res) != n:
+                res += str(m[i])
+        # print(getDigit(3,3))
+        return res
+        # return getDigit(k,n)
