24dc34d458ea9dd757276a789d37e7fbbd2f4d21
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-29 16:54:12 -0500

    Time: 0 ms (100%), Space: 18.1 MB (18.73%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -1,12 +1,6 @@
 class Solution:
     def getPermutation(self, n: int, k: int) -> str:
         fact = [1,1,2,6,24,120,720,5040,40320,362880]
-        # curr = 1
-        # for i in range(1, 9 + 1):
-        #     curr*= i
-        #     print(curr)
-        
-
         m = [x for x in range(10)]
 
         def getDigit(k, n):
@@ -24,6 +18,4 @@ class Solution:
         for i in range(len(m) - 1, 0, -1):
             if m[i] <= n and str(m[i]) not in res and len(res) != n:
                 res += str(m[i])
-        # print(getDigit(3,3))
         return res
-        # return getDigit(k,n)
