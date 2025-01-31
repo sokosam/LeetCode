a6b0b95896c2d7861c763a9e4771dc5c08f3df54
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-01-31 00:54:57 -0500

    Time: 1532 ms (17.87%), Space: 30.3 MB (45.95%) - LeetHub

---
+++ 0827-making-a-large-island/0827-making-a-large-island.py
@@ -0,0 +1,61 @@
+class Solution:     
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        
+        if grid == [[0]]:
+            return 1
+        dirs = [(1,0), (0,1), (-1,0), (0,-1)]
+        # group = [[0 for _ in range(len(grid[0]))] for _ in range(len(grid))]
+        currGroup = [2]
+        size = [0, 0]
+        def getSize(row,col):
+            
+            count = 0
+            q = deque()
+            q.append((row,col))
+
+
+            while q:
+                row, col = q.popleft()
+                if grid[row][col] != 1:
+                    continue
+                grid[row][col] = currGroup[0]
+                count += 1
+
+                for dx, dy in dirs:
+                    if self._isValid(row + dx, col + dy, grid) and grid[row +dx][col+dy] == 1:
+                        q.append((row+dx,col+dy))
+            size.append(count)
+            currGroup[0] +=1
+
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 1:
+                    getSize(row,col)
+        ans = 0
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == 0:
+                    groups = set()
+                    for dx, dy in dirs:
+                        if self._isValid(row +dx,col +dy,grid):
+                            groups.add(grid[row +dx][col + dy])
+                    val = 1
+                    for group in groups:
+                        val += size[group]
+                    ans = max(val, ans)
+                    if len(groups) == 1:
+                        ans = max(ans, size[list(groups)[0]] + 1)
+        
+
+        if ans == 0:
+            return max(size)
+        
+        print(grid)
+
+        print(size)
+        return ans
+
+    def _isValid(self,row,col,grid):
+        return row >=0 and row < len(grid) and col >= 0 and col < len(grid[0])
+                
+
