d7c9614fe9001b49d0ba7446bf865301ea07917f
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-02-11 20:52:02 -0500

    Time: 596 ms (11.53%), Space: 33.7 MB (52.32%) - LeetHub

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/2342-max-sum-of-a-pair-with-equal-sum-of-digits.py
@@ -3,11 +3,9 @@ class Solution:
         
 
         def getDigitSum(x):
-            
             x = str(x)
             ans =0
             for i in range(len(x) -1, -1, -1):
-
                 ans += int(x[i])
             return ans
         
@@ -18,7 +16,6 @@ class Solution:
 
         for i in range(len(nums)):
             digSum = getDigitSum(nums[i])
-
             if digSum not in m:
                 m[digSum] = nums[i]
             else:
