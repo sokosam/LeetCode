430ff3c3e2f7d9f6db554a60bdbd61f6e1a1dcaa
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-02-13 00:06:30 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3356-zero-array-transformation-ii/3356-zero-array-transformation-ii.py
@@ -0,0 +1,40 @@
+class Solution:
+    def minZeroArray(self, nums: List[int], queries: List[List[int]]) -> int:
+
+
+        def check(nums, queries):
+            pref = [0]* (len(nums) + 1)
+
+            for query in queries:
+                start = query[0]
+                end = query[1] + 1
+                val = query[2]
+                pref[start] -= val
+                pref[end] += val
+
+            curr = 0
+            for i in range(len(nums)):
+
+                curr += pref[i]
+                if nums[i] + curr > 0:
+                    return False
+            return True
+
+        ans = -1
+
+        l = 0
+        r = len(queries) - 1
+
+        while l <= r:
+            m = l + (r-l)//2
+
+
+            veri = check(nums, queries[0:m + 1 ])
+
+            if veri:
+                ans = min(ans,m + 1 ) if ans != -1 else m + 1 
+                print(m)
+                r = m -1
+            else:
+                l = m + 1
+        return ans
