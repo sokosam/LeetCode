9954f3fe80958a8d18c3eb610e6cba768c19e2f1
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-02-13 20:47:00 -0500

    Time: 0 ms (100%), Space: 18.1 MB (61.19%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,26 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        l = 0
+        r = max(citations)
+
+        def verify(citations, h):
+            count = 0 
+            for i in citations:
+                if i >= h: count +=1
+            return count >= h 
+        
+
+        ans = 0
+        while l <= r:
+            m = l + (r-l)//2
+
+            check = verify(citations,m)
+            # print(check)
+            if check:
+                l = m + 1
+                ans = max(ans, m)
+            else:
+                r = m -1
+        return ans
+            
