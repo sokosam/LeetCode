6820ad49a636ce7a79a1ccf42ef2323b6619ca17
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-02-16 02:52:17 -0500

    Time: 83 ms (42.64%), Space: 23.5 MB (45.81%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.py
@@ -0,0 +1,20 @@
+class Solution:
+
+    def __init__(self, w: List[int]):
+        self.sum = []
+        for i in w:
+            if len(self.sum) > 0:
+                self.sum.append(self.sum[-1] + i)
+            else:
+                self.sum.append(i)
+        print(self.sum)
+    def pickIndex(self) -> int:
+        r  = random.randint(1, self.sum[-1])
+        print(r)
+        return bisect.bisect_left(self.sum, r)
+        
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(w)
+# param_1 = obj.pickIndex()
