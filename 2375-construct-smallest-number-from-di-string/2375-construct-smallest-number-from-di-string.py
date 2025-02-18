43c3b242e0fb1d95d9898cb87c19e679c9ffbccf
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-02-17 19:42:38 -0500

    Time: 1 ms (28.16%), Space: 17.7 MB (83.23%) - LeetHub

---
+++ 2375-construct-smallest-number-from-di-string/2375-construct-smallest-number-from-di-string.py
@@ -0,0 +1,41 @@
+class Solution:
+    def smallestNumber(self, pattern: str) -> str:
+        
+        vals = [1]*10
+        s = []
+        ans = []
+
+        def bt(i, s):
+            if i >= len(pattern):
+                a = ""
+                print(s)
+                for i in s:
+                    a += str(i)
+
+                ans.append(a)
+                return True
+            
+            for k in range(1, 10):
+
+                if pattern[i] == 'I' and k <= s[-1]: continue
+                if pattern[i] == 'D' and k >= s[-1]: continue
+                if vals[k] == 0: continue
+                s.append(k)
+                vals[k] -= 1 
+                if bt(i + 1, s): 
+                    return True
+                s.pop()
+                vals[k] += 1
+            
+            return False
+        
+        for i in range(1, 10):
+            vals[i] -= 1
+            s.append(i)
+            if bt(0, s):
+                return ans[0]
+            s.pop()
+            vals[i] += 1
+
+        return ""
+
