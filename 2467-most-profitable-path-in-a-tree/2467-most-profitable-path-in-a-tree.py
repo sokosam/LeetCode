709cbec556bf5f315917fd32e7e97047ea93ff56
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-02-23 23:12:17 -0500

    Time: 3100 ms (5.08%), Space: 60.8 MB (66.44%) - LeetHub

---
+++ 2467-most-profitable-path-in-a-tree/2467-most-profitable-path-in-a-tree.py
@@ -0,0 +1,57 @@
+class Solution:
+    def mostProfitablePath(self, edges: List[List[int]], bob: int, amount: List[int]) -> int:
+        
+        adj = [[] for _ in range(len(amount))]
+        time = [-1] * len(amount)
+
+
+        for edge in edges:
+            adj[edge[0]].append(edge[1])
+            adj[edge[1]].append(edge[0])
+
+
+        q = deque()
+
+        visited = [False]*len(amount)
+        path = [bob]
+        q.append((bob, path))
+
+        while q:
+            curr, path = q.popleft()
+            visited[curr] = True
+
+            if curr == 0:
+                break
+
+            for i in adj[curr]:
+                if not visited[i]:
+                    q.append((i, path + [i]))
+        
+        for i in range(len(path)):
+            time[path[i]] = i
+
+        print(time)
+        visited = [False]*len(amount)
+        def dfs(t, curr, amt):
+
+            visited[curr] = True
+            if time[curr] == -1:
+                amt += amount[curr]
+            elif t < time[curr]:
+                amt += amount[curr]
+            elif t == time[curr]:
+                amt += amount[curr]//2
+
+            ans= float('-inf')
+            found= False
+            print(amt, curr, time[curr], t)
+            for vert in adj[curr]:
+                if not visited[vert]:
+                    found = True
+                    ans = max(ans, dfs(t + 1, vert, amt))
+
+            if not found:
+                return amt
+            return ans
+        
+        return dfs(0,0,0)
