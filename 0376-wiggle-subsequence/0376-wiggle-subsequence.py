cef83bc96b867421fa2b4285e35aafb2b681d25b
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-02-26 17:05:48 -0500

    Time: 0 ms (100%), Space: 18 MB (27.02%) - LeetHub

---
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.py
@@ -0,0 +1,40 @@
+class Solution:
+    def wiggleMaxLength(self, nums: List[int]) -> int:
+        
+        
+        
+
+        " 1 17 5 15 5 16 8"
+        " + -  + -  +  - "
+
+        "16, -12, 5, -5, 11, -8"
+
+        """
+
+
+        """
+        ans = []
+        i = 0
+        while i < len(nums):
+            if i == 0:
+                ans.append(nums[i])
+                i += 1
+                continue
+            while i < len(nums) and nums[i] == ans[-1]:
+                i += 1
+            if i >= len(nums): break
+            if len(ans) == 1:
+                ans.append(nums[i])
+            elif (nums[i] > ans[-1] > ans[-2]) or (ans[-2] > ans[-1] > nums[i]):
+                ans.pop()
+                ans.append(nums[i])
+            else:
+                ans.append(nums[i])
+            i+=1
+        print(ans)
+
+
+        
+
+            
+        return len(ans)
