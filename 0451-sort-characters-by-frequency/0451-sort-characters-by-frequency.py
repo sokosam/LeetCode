e8e78678024cf5448c844804ea9597482e510b9f
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-03-03 14:40:50 -0500

    Time: 22 ms (17.99%), Space: 18.7 MB (95.16%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.py
@@ -3,13 +3,19 @@ class Solution:
         
 
         vals = [[0, chr(x + ord('a')) ] for x in range(26)]
-        temp = [[0, chr(x + ord('A')) ] for x in range(26)]
-        vals.extend(temp)
+        upper = [[0, chr(x + ord('A')) ] for x in range(26)]
+        digits = [[0 , str(x)] for x in range(10)]
+        vals.extend(upper)
+        vals.extend(digits)
 
         for i in s:
-            index = ord(i) - ord('a')
-            if index < 0:
+            index = 0
+            if i.isnumeric():
+                index = 52 + int(i)
+            elif  ord(i) - ord('a') < 0:
                 index = ord(i) - ord('A') + 26
+            else:
+                index = ord(i) - ord('a')
             vals[index][0] -= 1
 
         heapq.heapify(vals)
