a68a3759472bdb167b541ddc8fa1d3ca1e329d50
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-06-07 17:23:32 -0400

    Time: 387 ms (87.01%), Space: 24.3 MB (49.35%) - LeetHub

---
+++ 3170-lexicographically-minimum-string-after-removing-stars/3170-lexicographically-minimum-string-after-removing-stars.py
@@ -0,0 +1,67 @@
+class Solution:
+    def clearStars(self, s: str) -> str:
+
+        count = [[] for _ in range(26)]
+        h = []
+        for i in range(len(s)):
+            if s[i] != "*":
+                value = s[i]
+                ordVal = ord(value) - ord('a')
+                count[ordVal].append(i)
+                if len(count[ordVal]) == 1:
+                    heapq.heappush(h, value)
+            else:
+                small = heapq.heappop(h)
+                smallest = ord(small) - ord("a")
+                count[smallest].pop()
+                if len(count[smallest]) != 0:
+                    heapq.heappush(h, small)
+        ans = ["*"]*len(s)
+
+        print(count)
+        for i in range(len(count)):
+            for k in count[i]:
+                ans[k] = chr(i + ord('a'))
+
+        z = ""
+
+        for i in ans:
+            if i != "*":
+                z += i
+        return z
+            
+        # c = {}
+        # for i in range(len(s)):
+        #     if s[i] not in c:
+        #         c[s[i]] = [i]
+        #     else:
+        #         c[s[i]].append(i)
+        
+        # heap = [[i, c[i]] for i in c if i != "*"]
+        # heapq.heapify(heap)
+        
+        # if "*" not in c:
+        #     return s
+        # amt = len(c["*"])
+
+        # while amt > 0:
+        #     item = heapq.heappop(heap)
+
+        #     item[1].pop()
+        #     if len(item[1]) != 0:
+        #         heapq.heappush(heap,item)
+        #     amt -=1
+        
+        # ans = ["*"]*len(s)
+
+        # while heap:
+        #     item = heapq.heappop(heap)
+        #     for i in item[1]:
+        #         ans[i] = item[0]
+        
+        # s = ""
+        # for i in ans:
+        #     if i != "*":
+        #         s += i
+        # return s
+            
