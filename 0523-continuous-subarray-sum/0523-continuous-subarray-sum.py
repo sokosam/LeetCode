2c41b60f330ce701626efebfac021aef071c15b8
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-06-18 19:50:46 -0400

    Time: 165 ms (5.05%), Space: 48.1 MB (5.19%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,35 +1,36 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-        
         """
-
-        23, 2, 4 , 6 , 6
-        k = 7
-
-        23 25 29 35 41
-
-        23 2 4 6 7 
-        k = 6
-
         23 25 29 35 42
-
-        23 % 6 = 5
-        25 % 6 = 1
-        29 % 6 = 5
-
-        
+        23  2  4  6  7
+    %k   5  1  5  5  0 
         """
-        if len(nums) == 1:
+
+        if len(nums) < 2:
             return False
-        
 
+        prefix = 0
+        m = {}
 
-        curr = nums[0]
-        m = {nums[0] % k : 0}
-        for i in range(1, len(nums)):
-            curr += nums[i]
-            if curr % k == 0 or (curr % k in m and i - m[curr%k]  >= 2 ):
+        for index, i in enumerate(nums):
+            prefix += i
+            if i == 0:
+                if index + 1 < len(nums):
+                    if nums[index + 1] % k == 0:
+                        return True
+                if index != 0:
+                    if nums[index - 1] % k == 0:
+                        return True
+                continue
+            mod = prefix % k
+            if mod == 0 and index != 0:
                 return True
-            if curr % k not in m:
-                m[curr% k] = i
+            if mod in m:
+                for i in range(len(m[mod]) - 1, -1,-1):
+                    if abs(m[mod][i] - index) >= 2:
+                        return True
+                m[mod].append(index)
+            else:
+                m[mod] = [index]
+        print(m)
         return False
