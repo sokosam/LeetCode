fd325af5b72f5c8f26c81546fc548ae5e74ac71d
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-06-28 22:27:22 -0400

    Time: 39 ms (31.14%), Space: 55.5 MB (5.5%) - LeetHub

--- 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
+++ 1899-merge-triplets-to-form-target-triplet/1899-merge-triplets-to-form-target-triplet.py
@@ -1,4 +1,3 @@
 class Solution:
     def mergeTriplets(self, triplets: List[List[int]], target: List[int]) -> bool:
-        
             return all([any([True for i in triplets if i[0] == target[0] and i[1] <= target[1] and i[2] <= target[2]]),any([True for i in triplets if i[0] <= target[0] and i[1] == target[1] and i[2] <= target[2]]),any([True for i in triplets if i[0] <= target[0] and i[1] <= target[1] and i[2] == target[2]])])
