c2abcbd50b31522aeca657b75aa9ca6b4c76b84b
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-07-25 23:42:12 -0400

    Time: 269 ms (5.06%), Space: 21.5 MB (8.79%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.py
+++ 0072-edit-distance/0072-edit-distance.py
@@ -1,17 +1,13 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         
-        if len(word2) == 0:
-            return len(word1)
-        if len(word1) == 0:
-            return len(word2)
-        dp = [[float('inf') for _ in range(len(word1) + 1)]for k in range(len(word2) + 1)]
-        for row in range(len(dp)):
-            for col in range(len(dp[0])):
-                if row == len(word2) :
-                    dp[row][col] = len(word1) - col
-                if col == len(word1) :
-                    dp[row][col] = len(word2) - row
+
+        m, n = len(word2), len(word1)
+        dp = [[float('inf')] * (n + 1) for _ in range(m + 1)]
+        dp[m] = [n - i for i in range(n + 1)]
+        for j in range(m + 1):
+            dp[j][n] = m - j
+
 
         def helper(i,j):
             if dp[j][i] != float('inf'):
