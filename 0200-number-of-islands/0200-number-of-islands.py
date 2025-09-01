52d9c5bdef3d746119f4119ed0872d267839701f
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-09-01 14:54:05 -0400

    Time: 261 ms (39.05%), Space: 20.2 MB (58.19%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -1,27 +1,19 @@
 class Solution:
+
+
+    def destroyIsland(self, row,col, grid):
+        if row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0]) and grid[row][col] == "1":
+            dirs = [(1,0), (0,1), (-1, 0), (0,-1)]
+            grid[row][col] = "0"
+            for dx,dy in dirs:
+                self.destroyIsland(row + dx, col + dy, grid)
     def numIslands(self, grid: List[List[str]]) -> int:
-        count = 0
-
-        maxRows = len(grid) -1 
-        maxCols = len(grid[0]) - 1
-
-        def destroyIsland(row, col):
-            if row > maxRows or row < 0:
-                return
-            if col > maxCols or col < 0:
-                return
-            if grid[row][col] != '1':
-                return
-            else:
-                grid[row][col] = '0'
-                destroyIsland(row + 1, col)
-                destroyIsland(row -1, col)
-                destroyIsland(row , col -1)
-                destroyIsland(row, col + 1)
-        
+        ans = 0
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == '1':
-                    destroyIsland(row,col)
-                    count +=1
-        return count
+                if grid[row][col] == "1":
+                    self.destroyIsland(row,col,grid)
+
+                    ans +=1
+        return ans
+        
