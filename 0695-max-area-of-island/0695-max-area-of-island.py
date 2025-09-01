86c508b15c0488772aee1b19f7a4233d0e9e13a3
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-09-01 14:59:45 -0400

    Time: 39 ms (14.89%), Space: 18.5 MB (33.41%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,22 +1,30 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
-        ans = 0
-        maxRow = len(grid) -1 
-        maxCol = len(grid[0]) - 1
-        def findSize(row,col):
-            if row > maxRow or row < 0:
-                return 0
-            if col > maxCol or col < 0:
+        
+        def isWithin(row,col, grid):
+            return row >= 0 and row < len(grid) and col >= 0 and col < len(grid[0])
+
+        def getArea(row, col, grid):
+            if not isWithin(row,col,grid):
                 return 0
             if grid[row][col] == 0:
                 return 0
-            else:
+            
+            dirs = [(1,0), (0,1) , (-1,0) , (0,-1)]
+
+            ans = 0
+            if grid[row][col] == 1:
+                ans +=1
                 grid[row][col] = 0
-                return findSize(row +1, col) + findSize(row -1, col) + findSize(row, col + 1) + findSize(row, col - 1) + 1
+            
+            for dx,dy in dirs:
+                ans += getArea(row + dx, col + dy, grid)
+            
+            return ans
+        
+        bestArea = 0
 
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == 1:
-                    size = findSize(row,col)
-                    ans = max(size,ans)
-        return ans
+                bestArea = max(bestArea, getArea( row, col, grid))
+        return bestArea
