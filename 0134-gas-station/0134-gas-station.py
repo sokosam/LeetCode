c30a810745b32550917c1e4c2d0c3f1f38798cca
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-09-14 18:33:24 -0400

    Time: 15 ms (88.93%), Space: 23.3 MB (83.11%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,38 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        """
+        # If total gas < total cost, impossible.
+        if sum(gas) < sum(cost):
+            return -1
 
-        [3, 7, 12, 13, 15]
-        [2,  4]
-
-        """
-
-        stuck = -1
-
-        station = 0
-        while station < len(gas):
-            temp = station
-
-            currGas = gas[station]
-
-            nextStation = cost[(temp) % len(gas)]
-            if currGas < nextStation:
-                station +=1
-                continue
-            else:
-                currGas -= cost[temp]
-                temp = (temp +1)%len(gas)
-                currGas += gas[temp]
-                while currGas >= cost[temp] and temp != station:
-                    currGas -= cost[temp]
-                    temp = (temp +1)%len(gas)
-                    currGas +=gas[temp]
-                
-                if temp == station:
-                    return station
-                else:
-                    if temp < station:
-                        return -1
-                    else:
-                        station = temp
-        return -1
+        start = 0
+        tank = 0
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                # Can't reach i+1 from current start; next start is i+1
+                start = i + 1
+                tank = 0
+        return start
