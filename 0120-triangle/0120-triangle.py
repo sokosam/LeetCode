fc6f0a122fa4a0552014b488dd73222d7b4c206b
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-09-25 09:31:28 -0400

    Time: 7 ms (33.7%), Space: 18.9 MB (62.18%) - LeetHub

--- 0120-triangle/0120-triangle.py
+++ 0120-triangle/0120-triangle.py
@@ -1,23 +1,10 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         
-        ans = float('inf')
-        for i in range(len(triangle)):
 
-            if i == 0:
-                curr_min = triangle[i][0]
-                ans = curr_min
-                continue
-            curr_min = float('inf')
+        for i in range(1, len(triangle)):
             for j in range(len(triangle[i])):
-                left = float('inf')
-                right = float('inf')
-                if j -1 >= 0:
-                    left = triangle[i - 1][j -1]
-                if j  < len(triangle[i-1]):
-                    right = triangle[i-1][j]
-                
-                triangle[i][j] = min(triangle[i][j] + left, triangle[i][j] + right)
-                curr_min = min(curr_min, triangle[i][j])
-            ans = curr_min
-        return ans
+                left = triangle[i-1][j - 1] if j -1 >= 0 else float('inf')
+                right = triangle[i-1][j] if j < len(triangle[i - 1]) else float('inf')
+                triangle[i][j] = min(left, right) + triangle[i][j]
+        return min(triangle[-1])
