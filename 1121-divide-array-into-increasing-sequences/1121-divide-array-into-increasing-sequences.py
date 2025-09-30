4dcbb9d0412809a1de87b07aef8cab6aa17fd5d4
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-09-30 13:50:27 -0400

    Time: 31 ms (71.88%), Space: 32.4 MB (62.5%) - LeetHub

--- 1121-divide-array-into-increasing-sequences/1121-divide-array-into-increasing-sequences.py
+++ 1121-divide-array-into-increasing-sequences/1121-divide-array-into-increasing-sequences.py
@@ -3,19 +3,4 @@ class Solution:
         h = []
         m = Counter(nums)
         maxSize = max(m.values())
-
-        for i in nums:
-            if len(h) < maxSize:
-                heappush(h, [i, 1])
-            else:
-                best = h[0]
-                if best == i:
-                    return False
-                else:
-                    x = heappop(h)
-                    heappush(h, [i, x[1] + 1])
-        
-        for i in h:
-            if i[1] < k:
-                return False
-        return True
+        return  len(nums)//maxSize >= k
