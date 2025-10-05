24c892b446201d4b58deb2d4bf65ecc50ae404f4
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-10-05 16:50:18 -0400

    Time: 36 ms (44.84%), Space: 19.5 MB (44.54%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,36 +1,65 @@
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        atlantic = [[False for _ in range(len(heights[0]))] for _ in range(len(heights))]  
-        pacific = [[False for _ in range(len(heights[0]))] for _ in range(len(heights))]  
-        print(atlantic)
-        print(pacific)
-        def check(ocean, row, col):
-            if ocean[row][col]:
-                return
-            
-            ocean[row][col] = True
-
-            if row + 1 < len(heights) and heights[row + 1][col] >= heights[row][col]:
-                check(ocean, row + 1, col)
-            if row - 1 >= 0 and heights[row -1][col] >= heights[row][col]:
-                check(ocean, row - 1, col)
-            if col + 1 < len(heights[0]) and heights[row][col + 1] >= heights[row][col]:
-                check(ocean, row, col +1)
-            if col - 1 >= 0 and heights[row][col -1] >= heights[row][col]:
-                check(ocean, row, col -1)
-            
         
-        for i in range(len(heights[0])):
-            check(pacific,0, i)
-            check(atlantic, len(heights) - 1, i)
+
+        aSeen = set()
+        pSeen = set()
+        atlantic = [[False for _ in range(len(heights[0]))] for _ in range(len(heights))]
+        pacific = [[False for _ in range(len(heights[0]))] for _ in range(len(heights))]
+
+
+        q1 = deque()
 
         for i in range(len(heights)):
-            check(pacific, i, 0)
-            check(atlantic, i, len(heights[0]) - 1)
+            q1.append((i, len(heights[0]) - 1 ))
+            aSeen.add((i, len(heights[0]) - 1 ))
+        
+        for i in range(len(heights[0])):
+            q1.append((len(heights) -1 , i))
+            aSeen.add((len(heights) -1 , i))
+
+
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+        while q1:
+            curr = q1.popleft()
+            x,y = curr
+            atlantic[x][y] = True
+
+            for dx, dy in dirs:
+                newX,newY = dx+x,dy+y
+                if 0 <= newX < len(heights) and 0 <= newY < len(heights[0]):
+                    if heights[x][y] <= heights[newX][newY] and (newX,newY) not in aSeen:
+                        q1.append((newX,newY))
+                        aSeen.add((newX,newY))
+
+
+        q2 = deque()
+
+        for i in range(len(heights)):
+            q2.append((i, 0 ))
+            pSeen.add((i, 0))
+        
+        for i in range(len(heights[0])):
+            q2.append((0 , i))
+            pSeen.add((0, i))
+
+
+        dirs = [(0,1), (1,0), (-1,0), (0,-1)]
+        while q2:
+            curr = q2.popleft()
+            x,y = curr
+            pacific[x][y] = True
+
+            for dx, dy in dirs:
+                newX,newY = dx+x,dy+y
+                if 0 <= newX < len(heights) and 0 <= newY < len(heights[0]):
+                    if heights[x][y] <= heights[newX][newY] and (newX,newY) not in pSeen:
+                        q2.append((newX,newY))
+                        pSeen.add((newX,newY))
 
         ans = []
-        for row in range(len(heights)):
-            for col in range(len(heights[0])):
-                if atlantic[row][col] and pacific[row][col]:
-                    ans.append([row,col])
+        for x in range(len(heights)):
+            for y in range(len(heights[0])):
+                if pacific[x][y] and atlantic[x][y]:
+                    ans.append([x,y])
         return ans
