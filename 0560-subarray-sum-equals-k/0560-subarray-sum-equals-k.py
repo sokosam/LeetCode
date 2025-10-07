3b458f642d623a52a4cbe885dc478938fde6d12b
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-10-07 19:36:10 -0400

    Time: 39 ms (35.34%), Space: 20.3 MB (71.79%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.py
@@ -1,42 +1,19 @@
 class Solution:
     def subarraySum(self, nums: List[int], k: int) -> int:
-        # l = 0
-        # r  =0
-        # curr = 0
-        # ans = 0
-        # while r < len(nums):
-        #     curr += nums[r]
-        #     while curr > k and r != l:
-        #         curr -= nums[l]
-        #         l += 1
-            
-        #     if curr == k:
-        #         ans +=1
-        #     r +=1
-        # return ans
-
-        dp = {0 : 1}
-        ans = 0
-        curr = 0
-        for i in nums:
-            curr += i
-            if curr - k in dp:
-                ans += dp[curr - k]
-
-            if curr not in dp:
-                dp[curr] = 1
-            else:
-                dp[curr] +=1
-
-        return ans
         """
 
-        [-1, -1, 1]
+        1 2 3 
         
-        0 
-
-        -1 -2 -1 
-
-
 
         """
+
+        seen = defaultdict(int)
+        seen[0] =1
+        prefix = 0
+        ans = 0
+        for i in nums:
+            prefix += i
+            if prefix - k in seen:
+                ans += seen[prefix - k]
+            seen[prefix] += 1
+        return ans
