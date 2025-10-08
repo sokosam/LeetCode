c8ffe548186d36d099640e6280534200ff90cd35
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-10-08 11:03:02 -0400

    Time: 0 ms (100%), Space: 18.2 MB (41.75%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,39 +1,50 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
+        """
 
-        def binSearch(nums, target):
+            if the array is rotated, nums[0] > nums[-1]
+
+        """
+
+        def binSearch(target, nums):
+            
             l = 0
-            r = len(nums) - 1
-            while l <= r:
-                m = (l + r) //2
+            r = len(nums) -1 
+            while l<=r:
+                m = l + (r-l)//2
 
                 if nums[m] == target:
                     return m
-                elif nums[m] < target:
+                elif nums[m] > target:
+                    r = m -1 
+                else:
+                    l = m + 1
+            return -1
+
+        if nums[0] <= nums[-1]:
+            return binSearch(target, nums)
+
+       
+        def findPivot(nums):
+            maxElement = nums[0]
+            index = -1
+            l = 0
+            r = len(nums) - 1
+            while l<=r:
+                m = l + (r-l)//2
+                if nums[m] >= maxElement:
+                    maxElement = nums[m]
+                    index = m
                     l = m + 1
                 else:
                     r = m - 1
-            return -1
-        
-        if nums[0] < nums[len(nums) - 1]: return binSearch(nums, target)
-        l = 0
-        r = len(nums) - 1
-        m = 0
-        while l < r:
-            leftMost = nums[l]
-            rightMost = nums[r]
-            m = (l + r)//2
+            return index
+        pivot = findPivot(nums)
+        print(pivot)
 
-            if r - l == 1:
-                m = l
-                break            
-            if leftMost > nums[m]:
-                r = m 
-            if rightMost < nums[m]:
-                l = m 
-        left = binSearch(nums[0: m + 1], target)
-        right = binSearch(nums[m+1 :], target)
-        if right != -1:
-            right += m + 1
-        return max(left,right)
+        left = binSearch(target,nums[0: pivot + 1])
+        right = -1
+        if binSearch(target, nums[pivot+1:]) >= 0:
+            right = pivot + 1 + binSearch(target, nums[pivot+1:])
+        return max(left, right)
             
