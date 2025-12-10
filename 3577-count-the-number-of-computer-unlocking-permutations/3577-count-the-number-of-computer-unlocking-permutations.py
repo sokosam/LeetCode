f30ee6cce08acc666b0820d42211b4f0619326c2
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-12-09 20:27:29 -0500

    Time: 3123 ms (5.55%), Space: 51.7 MB (8.33%) - LeetHub

--- 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
+++ 3577-count-the-number-of-computer-unlocking-permutations/3577-count-the-number-of-computer-unlocking-permutations.py
@@ -6,14 +6,15 @@ class Solution:
         m = Counter(complexity)
         if m[complexity[0]] > 1:
             return 0
+
+
+
         def fact(x):
             if x == 0 or x ==1:
                 return 1
             return x*fact(x-1)
         ans = fact(len(complexity) - 1)
-        for i in m :
-            if m[i] > 1:
-                ans//fact(m[i])
+
 
 
 
