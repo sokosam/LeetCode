d5c2ce589ad94973785b0c6817d7189adb755ac5
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-12-13 22:05:11 -0500

    Time: 443 ms (30.48%), Space: 18.4 MB (76.19%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,11 +1,5 @@
 class Solution:
     def numberOfWays(self, corridor: str) -> int:
-        m =Counter(corridor)
-        if "S" not in m or m["S"] % 2 ==1:
-            return 0 
-        if m["S"] == 2:
-            return 1
-        m = defaultdict(int)
 
         ptr = 0
         chair_count = 0
@@ -20,8 +14,6 @@ class Solution:
                     total*= (ptr - last_seen) % MOD
                 last_seen = ptr
             ptr +=1
+        if chair_count < 2 or chair_count % 2 == 1:
+            return 0
         return total% MOD
-        ans = 1
-        for num in m:
-            ans *= (num)**m[num] 
-        return ans% MOD
