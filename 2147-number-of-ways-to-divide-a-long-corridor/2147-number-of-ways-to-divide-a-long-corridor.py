d0caae02080dcf08b1952a2a3306c7983ffcf4cb
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2025-12-13 22:04:23 -0500

    Time: 567 ms (25.71%), Space: 18.7 MB (62.86%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -10,15 +10,17 @@ class Solution:
         ptr = 0
         chair_count = 0
         last_seen = 0
+        total = 1
+        MOD = 10**9 + 7
 
         while ptr < len(corridor):
             if corridor[ptr] == "S":
                 chair_count += 1
                 if chair_count > 2 and chair_count % 2 == 1:
-                    m[ptr - last_seen ] +=1
+                    total*= (ptr - last_seen) % MOD
                 last_seen = ptr
             ptr +=1
-        MOD = 10**9 + 7
+        return total% MOD
         ans = 1
         for num in m:
             ans *= (num)**m[num] 
