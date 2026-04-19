636404be199c7b6378144c92a357cc81b0bdece5
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2026-04-19 18:56:43 -0400

    Time: 63 ms (43.15%), Space: 35.6 MB (96.92%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.py
@@ -6,7 +6,6 @@ class Solution:
 
         for i in range(len(nums1)):
             n1 = nums1[i]
-            
             while ptr2 < len(nums2) and nums2[ptr2] >= n1 :
                 ptr2 += 1
             farthest = max(farthest, ptr2 - i - 1)
