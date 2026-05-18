f87a66b419ae82040df1f6748b9e4b00d19c5152
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2026-05-17 19:32:13 -0700

    Time: 103 ms (98.52%), Space: 52.6 MB (8.3%) - LeetHub

---
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -0,0 +1,37 @@
+
+
+class SnapshotArray:
+
+    def __init__(self, length: int):
+        self.track = [{-1: 0} for _ in range(length)]
+        self.total_snaps = 0
+
+    def set(self, index: int, val: int) -> None:
+        self.track[index][self.total_snaps ] = val
+        
+
+    def snap(self) -> int:
+        self.total_snaps +=1
+        return self.total_snaps - 1
+
+    def get_max(self, index, snap_id):
+        best = -1
+        for i in self.track[index].keys():
+            if i > best and i <= snap_id:
+                best = i
+        return best
+
+
+
+    def get(self, index: int, snap_id: int) -> int:
+        if snap_id not in self.track[index]:
+            return self.track[index][self.get_max(index,snap_id) ]
+        return self.track[index][snap_id]
+        
+
+
+# Your SnapshotArray object will be instantiated and called as such:
+# obj = SnapshotArray(length)
+# obj.set(index,val)
+# param_2 = obj.snap()
+# param_3 = obj.get(index,snap_id)
