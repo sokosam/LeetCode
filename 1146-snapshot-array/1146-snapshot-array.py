4f0951d29b783f73206052a265bb366bee662085
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2026-05-17 19:48:52 -0700

    Time: 96 ms (99.01%), Space: 47.1 MB (65.32%) - LeetHub

--- 1146-snapshot-array/1146-snapshot-array.py
+++ 1146-snapshot-array/1146-snapshot-array.py
@@ -3,13 +3,12 @@
 class SnapshotArray:
 
     def __init__(self, length: int):
-        self.track = [{-1: 0} for _ in range(length)]
+        self.track = [{} for _ in range(length)]
         self.total_snaps = 0
 
     def set(self, index: int, val: int) -> None:
         self.track[index][self.total_snaps ] = val
         
-
     def snap(self) -> int:
         self.total_snaps +=1
         return self.total_snaps - 1
@@ -23,7 +22,10 @@ class SnapshotArray:
 
     def get(self, index: int, snap_id: int) -> int:
         if snap_id not in self.track[index]:
-            return self.track[index][self.get_max(index,snap_id) ]
+            bid = self.get_max(index,snap_id)
+            if bid == -1:
+                return 0
+            return self.track[index][bid]
         return self.track[index][snap_id]
         
 
