a40291c54b274c12161ebc9c33d6a2cae594afbd
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2026-06-09 21:00:54 -0700

    Time: 16 ms (84.62%), Space: 58.9 MB (5.13%) - LeetHub

---
+++ 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.py
@@ -0,0 +1,24 @@
+class Solution:
+    def minRectanglesToCoverPoints(self, points: List[List[int]], w: int) -> int:
+        """
+        height is irrelevant
+
+        greedy seems to work
+
+        """
+
+        points = list(set([points[i][0] for i in range(len(points))]))
+        points.sort()
+        total = 0
+        for i in range(len(points)):
+            if i == 0:
+                farthest = points[i] + w
+                total += 1
+                continue
+            x = points[i]
+            if x > farthest:
+                farthest = x + w
+                total +=1
+        return total
+            
+
