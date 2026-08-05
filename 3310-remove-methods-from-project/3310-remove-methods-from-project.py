058681d5fcb03eb2447ad1441ff561556e84c392
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2026-08-04 21:38:28 -0700

    Time: 340 ms (50%), Space: 99.9 MB (83.33%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.py
@@ -0,0 +1,30 @@
+class Solution:
+    def remainingMethods(self, n: int, k: int, invocations: List[List[int]]) -> List[int]:
+        indir = [0]*n
+        adj = [[] for _ in range(n)]
+        sus = [False]*n
+        for outer, inner in invocations:
+            adj[outer].append(inner)
+            indir[inner] += 1
+        q = deque()
+
+
+        q.append(k)
+        while q:
+            curr = q.popleft()
+            sus[curr] = True
+
+            for i in adj[curr]:
+                if not sus[i]:
+                    sus[curr] = True
+                    q.append(i)
+        
+        for i in range(n):
+            if not sus[i]:
+                for j in adj[i]:
+                    if sus[j]:
+                        return [k for k in range(n)] 
+        
+        return [i for i in range(n) if not sus[i]]
+
+                
