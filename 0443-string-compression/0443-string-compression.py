75f6d9adf2d6b68eb3aa5b3bb9a2adf106a994ce
Samuel Shi <138630438+sokosam@users.noreply.github.com>
2026-09-15 18:29:41 -0400

    Time: 4 ms (14.42%), Space: 19.6 MB (20.48%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -4,14 +4,11 @@ class Solution:
         compared_char = chars[0]
         count = 1
         for i in range(1, len(chars)):
-            print(char_ptr, compared_char)
             if chars[i] == compared_char:
                 count += 1
             else:
                 if count != 1:
-                    print(compared_char, 'test')
                     str_count = str(count)
-                    print(str_count)
                     for j in range(1, 1 + len(str_count)):
                         chars[char_ptr + j] = str_count[j - 1]
 
